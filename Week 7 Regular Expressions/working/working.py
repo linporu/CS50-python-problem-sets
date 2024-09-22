3bb046b88705d2e1f7dc705822961208a15071a1
linporu <qazwsx123456guy@gmail.com>
2024-09-22 21:58:51 +0800

    Reconstruct week 7 problem set "working.py" Devide convert() into small pieces to increase readability and maintainability.

--- Week 7 Regular Expressions/working/working.py
+++ Week 7 Regular Expressions/working/working.py
@@ -9,71 +9,39 @@ def convert(s):
     pattern = r"^(?P<start>(\d(:[0-5]\d)?|(1[0-1])(:[0-5]\d)?|12(:00)?) [AP]M) to (?P<end>(\d(:[0-5]\d)?|1[0-1](:[0-5]\d)?|12(:00)?) [AP]M)$"
 
     if matches := re.search(pattern, s):
-        start_time, start_AMPM = matches.group("start").split()
-        end_time, end_AMPM = matches.group("end").split()
 
-        if ":" in start_time:
+        start_hr, start_min, start_AM_or_PM = parse_string(matches.group("start"))
+        start_hr = convert_hr(start_hr, start_AM_or_PM)
 
-            if start_AMPM == "AM":
-                if start_time.split(":")[0] != "12":
-                    start_hr = int(start_time.split(":")[0])
-                else:
-                    start_hr = 0
-
-            elif start_AMPM == "PM":
-                if start_time.split(":")[0] != "12":
-                    start_hr = int(start_time.split(":")[0]) + 12
-                else:
-                    start_hr = 12
-
-            start_min = int(start_time.split(":")[1])
-
-        else:
-
-            if start_AMPM == "AM":
-                if start_time != "12":
-                    start_hr = int(start_time)
-                else:
-                    start_hr = 0
-            elif start_AMPM == "PM":
-                if start_time != "12":
-                    start_hr = int(start_time) + 12
-                else:
-                    start_time = 0
-
-            start_min = 0
-
-        if ":" in end_time:
-            if end_AMPM == "AM":
-                if end_time.split(":")[0] != "12":
-                    end_hr = int(end_time.split(":")[0])
-                else:
-                    end_hr = 0
-            elif end_AMPM == "PM":
-                if end_time.split(":")[0] != "12":
-                    end_hr = int(end_time.split(":")[0]) + 12
-                else:
-                    end_hr = 12
-
-            end_min = int(end_time.split(":")[1])
-
-        else:
-            if end_AMPM == "AM":
-                if end_time != "12":
-                    end_hr = int(end_time)
-                else:
-                    end_hr = 0
-            elif end_AMPM == "PM":
-                if end_time != "12":
-                    end_hr = int(end_time) + 12
-                else:
-                    end_hr = 12
-
-            end_min = 0
+        end_hr, end_min, end_AM_or_PM = parse_string(matches.group("end"))
+        end_hr = convert_hr(end_hr, end_AM_or_PM)
 
         return f"{start_hr:02}:{start_min:02} to {end_hr:02}:{end_min:02}"
 
     raise ValueError
+
+
+def parse_string(time_string):
+    time, AM_or_PM = time_string.split()
+
+    if ":" in time:
+        hr, min = time.split(":")
+        return hr, min, AM_or_PM
+
+    else:
+        hr = time
+        min = 0
+        return hr, min, AM_or_PM
+
+
+def convert_hr(hr, AM_or_PM):
+    if AM_or_PM == "AM":
+        hr = int(hr) if hr != "12" else 0  # Handle 12:00 AM
+        return hr
+
+    else:
+        hr = int(hr) + 12 if hr != "12" else 12  # Handle 12:00 PM
+        return hr
 
 
 if __name__ == "__main__":
